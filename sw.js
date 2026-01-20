5f53d22e8ab571b9bb82ba0b7f419540cd93a483
benjaminbennett0 <benjamin.bennett0@gmail.com>
2026-01-20 16:11:25 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,29 @@
-const CACHE_NAME = 'timer-v1';
-const ASSETS = ['./index.html', './manifest.json'];
+const CACHE_NAME = 'timer-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json'
+];
 
 self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.map(key => {
+        if (key !== CACHE_NAME) return caches.delete(key);
+      })
+    ))
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
 });
